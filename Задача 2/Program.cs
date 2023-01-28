d59a0e262808ebd591e19abe5c001b5a6069362d
Irina <gammi_i@mail.ru>
2023-01-28 12:43:42 +0300

    Задача на нахождение max и min из двух чисел

---
+++ Задача 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.Clear();
+
+Console.WriteLine("Введите два числа ");
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+
+if (A > B)
+{
+    Console.WriteLine($"{A} - max");
+    Console.WriteLine($"{B} - min");
+}
+
+if (A == B)
+{
+    Console.WriteLine("Введите разные числа.");
+}
+
+else 
+
+{   
+    Console.WriteLine($"{A} - min");
+    Console.WriteLine($"{B} - max"); 
+}
