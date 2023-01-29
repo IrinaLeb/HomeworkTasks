b6e3632316fdb8a56cd4a230d4c2b94e09337e4d
Irina <gammi_i@mail.ru>
2023-01-29 12:36:52 +0300

    Нахождение max из трёх чисел

---
+++ Задача 4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.Clear();
+
+Console.WriteLine("Введите три числа ");
+int A = int.Parse (Console.ReadLine()!);
+int B = int.Parse (Console.ReadLine()!);
+int C = int.Parse (Console.ReadLine()!);
+
+if (A > B && A > C)
+{
+    Console.Write(A);
+}
+
+else if (B > C)
+{
+    Console.Write(B);
+}
+
+else 
+{
+    Console.Write(C);
+}
