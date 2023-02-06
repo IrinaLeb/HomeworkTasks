e5ec832fa32f20b9a6ce2575d19c1d69694d694b
Irina <gammi_i@mail.ru>
2023-02-06 19:36:37 +0300

    На входе два числа, на выходе проверяет, является ли одно число квадратом другого

---
+++ Задача 16/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №16. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// ● 5, 25 -> да
+// ● -4, 16 -> да
+// ● 25, 5 -> да
+// ● 8,9 -> нет
+Console.Clear();
+
+Console.WriteLine ("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine ("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+//if (B * B == A || A * A == B) 
+//if (Math.Pow (B, 2) == A || Math.Pow (A, 2) == B) 
+if (A % B == 0 || B % A == 0)
+{
+    Console.WriteLine("Да");    
+}
+else
+{
+    Console.WriteLine("Нет");
+}
