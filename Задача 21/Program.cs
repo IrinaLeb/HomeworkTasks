0bd26f9c15088b618ed0bfb666a05fa16a4ca31f
Irina <gammi_i@mail.ru>
2023-02-10 21:02:46 +0300

    На входе координаты двух точек, на выходе находит расстояние между ними в 2D пространстве

---
+++ Задача 21/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// ● A (3,6); B (2,1) -> 5,09
+// ● A (7,-5); B (1,-1) -> 7,21
+Console.Clear();
+
+Console.Write("Введите Хa: ");
+int Xa = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Хb: ");
+int Xb = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Ya: ");
+int Ya = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Yb: ");
+int Yb = int.Parse(Console.ReadLine()!);
+
+double Distans = Math.Sqrt(Math.Pow(Xa - Xb, 2) + Math.Pow(Ya - Yb, 2));
+Console.WriteLine($"Расстояние между точками А и B -> {Distans:f2}");
+ 
